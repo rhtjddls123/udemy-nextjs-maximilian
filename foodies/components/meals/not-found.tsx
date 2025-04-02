d5f24bef10dc851fd1f8095a187643f1a405a5d2
rhtjddls123 <jjjk0605@naver.com>
2025-04-02 17:12:20 +0900

    not found페이지 제작

---
+++ foodies/components/meals/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we cloud not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFound;

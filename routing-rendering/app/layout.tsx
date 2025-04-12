fcd02ed7c29e1bcc7976ce6386df9677dfe7e340
rhtjddls123 <jjjk0605@naver.com>
2025-04-12 15:45:14 +0900

    project init

---
+++ routing-rendering/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
